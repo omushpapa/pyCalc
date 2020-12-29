58662bd7d4056e48f7be8ed65b505c60c0a33cf3
Aswa Paul <aswapaul@gmail.com>
2020-12-29 22:21:09 +0300

    Use enums for operators
    Ignore .ini files

--- pyCalc/calculations.py
+++ pyCalc/calculations.py
@@ -4,45 +4,60 @@ import operator
 import tokenize
 import typing
 from decimal import Decimal
+from enum import Enum
 from io import StringIO
 
 logger = logging.getLogger(__name__)
 
 LEFT_PAREN = '('
 RIGHT_PAREN = ')'
-OPERATORS = {
-    '(': {
-        'precedence': 0,
-    },
-    '+': {
-        'precedence': 1,
-        'function': operator.add,
-    },
-    '-': {
-        'precedence': 1,
-        'function': operator.sub,
-    },
-    'neg': {
-        'precedence': 2,
-        'function': operator.neg,
-    },
-    '*': {
-        'precedence': 3,
-        'function': operator.mul,
-    },
-    '/': {
-        'precedence': 4,
-        'function': operator.truediv,
-    },
-    '%': {
-        'precedence': 5,
-        'function': lambda x: x / 100
-    },
-    '!': {
-        'precedence': 5,
-        'function': math.factorial,
-    }
-}
+
+
+class Operator(typing.NamedTuple):
+    symbol: str
+    precedence: int
+    function: typing.Callable = None
+
+
+class Operators(Enum):
+    LEFT_PARENTHESIS = Operator('(', 0)
+    ADDITION = Operator('+', 1, operator.add)
+    SUBTRACTION = Operator('-', 1, operator.sub)
+    MULTIPLICATION = Operator('*', 3, operator.mul)
+    DIVISION = Operator('/', 4, operator.truediv)
+    PERCENTAGE = Operator('%', 5, lambda x: x / 100)
+    FACTORIAL = Operator('!', 5, math.factorial)
+    NEGATION = Operator('neg', 6, operator.neg)
+
+    @classmethod
+    def from_symbol(cls, symbol):
+        for i in cls:
+            if i.symbol == symbol:
+                return i
+
+        raise ValueError('Invalid symbol')
+
+    @classmethod
+    def is_valid_symbol(cls, symbol):
+        exists = False
+        for i in cls:
+            if i.symbol == symbol:
+                exists = True
+                break
+
+        return exists
+
+    @property
+    def precedence(self):
+        return self.value.precedence
+
+    @property
+    def function(self):
+        return self.value.function
+
+    @property
+    def symbol(self):
+        return self.value.symbol
 
 
 def to_postfix(expression):
@@ -52,10 +67,7 @@ def to_postfix(expression):
     postfix = []
 
     for i, token in enumerate(tokens):
-        if not token.strip():
-            continue
-
-        if token == LEFT_PAREN:
+        if token == Operators.LEFT_PARENTHESIS:
             operations.append(token)
 
         elif token == RIGHT_PAREN:
@@ -64,8 +76,8 @@ def to_postfix(expression):
                 postfix.append(top_operation)
                 top_operation = operations.pop()
 
-        elif token in OPERATORS:
-            while operations and OPERATORS[token]['precedence'] <= OPERATORS[operations[-1]]['precedence']:
+        elif token in Operators:
+            while operations and token.precedence <= operations[-1].precedence:
                 top_operation = operations.pop()
                 postfix.append(top_operation)
 
@@ -91,18 +103,23 @@ def get_tokens(expression):
     final_tokens = []
     swap_indexes = []
     for i, token in enumerate(tokens):
-        token_string = token[1]
+        logger.debug(f'parsing token {token}')
+        token_string = token[1].strip() if token[1] else token[1]
         if token_string:
             if token_string == '-':
-                if not final_tokens or (final_tokens and final_tokens[-1] in OPERATORS):
+                if not final_tokens or (final_tokens and final_tokens[-1] in Operators):
                     swap_indexes.append(len(final_tokens))
 
-            final_tokens.append(token_string)
+            try:
+                float(token_string)
+            except ValueError:
+                final_tokens.append(Operators.from_symbol(token_string))
+            else:
+                final_tokens.append(token_string)
 
     for index in swap_indexes:
-        number = final_tokens[index + 1]
-        final_tokens[index] = number
-        final_tokens[index + 1] = 'neg'
+        final_tokens[index] = final_tokens[index + 1]
+        final_tokens[index + 1] = Operators.NEGATION
 
     return final_tokens
 
@@ -113,22 +130,20 @@ def evaluate(expression):
 
     results = []
     for token in operations:
-        if token in OPERATORS:
-            function: typing.Callable = OPERATORS[token]['function']
-
-            if token in ('neg', '%',):
+        if token in Operators:
+            if token in (Operators.NEGATION, Operators.PERCENTAGE,):
                 operand = results.pop()
-                result = function(Decimal(operand))
+                result = token.function(Decimal(operand))
 
-            elif token == '!':
+            elif token == Operators.FACTORIAL:
                 operand = results.pop()
-                result = function(float(operand))
+                result = token.function(float(operand))
 
             else:
                 second_operand = results.pop()
                 first_operand = results.pop()
 
-                result = function(Decimal(first_operand), Decimal(second_operand))
+                result = token.function(Decimal(first_operand), Decimal(second_operand))
 
             logger.debug(f'applied {token} to get {result}')
             results.append(result)
